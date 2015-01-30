015a190adb0bc6eedd8fbf31b3c4fa6010c541de
Tsinoyboi <ajsnoface@gmail.com>
2015-01-29 20:20:05 -0800

    new schema

--- sql/init.sql
+++ sql/init.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS user
 (
     id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY (id),
     UNIQUE KEY (username)
@@ -31,24 +32,15 @@ INSERT INTO face
     'tails'
 );
 
-CREATE TABLE IF NOT EXISTS streak
+CREATE TABLE IF NOT EXISTS flip
 (
     id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id BIGINT(20) UNSIGNED NOT NULL,
     face_id BIGINT(20) UNSIGNED NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY (id),
-    FOREIGN KEY (user_id) REFERENCES user (id),
-    FOREIGN KEY (face_id) REFERENCES face (id)
-) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS flip
-(
-    id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    streak_id BIGINT(20) UNSIGNED NOT NULL,
     time_flipped DATETIME NOT NULL,
     milliseconds INT(10) UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY (id),
-    FOREIGN KEY (streak_id) REFERENCES streak (id)
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (face_id) REFERENCES face (id)
 ) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci;
